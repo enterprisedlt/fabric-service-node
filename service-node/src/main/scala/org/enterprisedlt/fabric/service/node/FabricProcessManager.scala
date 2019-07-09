e5ba91484faf22b49ea0d036eb6c9df99200c7f2
Alexey Polubelov <42519530+apolubelov@users.noreply.github.com>
2019-07-10 01:31:36 +0300

    name for trait fixed

--- service-node/src/main/scala/org/enterprisedlt/fabric/service/node/FabricProcessManager.scala
+++ service-node/src/main/scala/org/enterprisedlt/fabric/service/node/FabricProcessManager.scala
@@ -3,7 +3,7 @@ package org.enterprisedlt.fabric.service.node
 /**
   * @author Alexey Polubelov
   */
-trait IFabricProcessManager {
+trait FabricProcessManager {
     def startOrderingNode(name: String, port: Int): String
     def awaitOrderingJoinedRaft(name: String)
     def startPeerNode(name: String, port: Int): String
