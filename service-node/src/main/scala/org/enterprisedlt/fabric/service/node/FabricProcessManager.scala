13a64982a4df01ec4d1a75a1f1c03ce92d605f7b
Andrew Pudovikov <pudovikov@me.com>
2019-07-15 17:45:48 +0300

    peer and orderer start methods have been refactored (got rid port variable in func arguments, instead port getting from network configuration) during peer start procedure network conf is readed and if there is couchdb option then it starts couchdb container at first, then peer.
    chmod +x * for all scripts in the root of project otherwise them don't start

--- service-node/src/main/scala/org/enterprisedlt/fabric/service/node/FabricProcessManager.scala
+++ service-node/src/main/scala/org/enterprisedlt/fabric/service/node/FabricProcessManager.scala
@@ -4,13 +4,13 @@ package org.enterprisedlt.fabric.service.node
   * @author Alexey Polubelov
   */
 trait FabricProcessManager {
-    def startOrderingNode(name: String, port: Int): String
+    def startOrderingNode(name: String): String
 
     def osnAwaitJoinedToRaft(name: String): Unit
 
     def osnAwaitJoinedToChannel(name: String, channelName: String): Unit
 
-    def startPeerNode(name: String, port: Int): String
+    def startPeerNode(name: String): String
 
     def peerAwaitForBlock(name: String, blockNumber: Long): Unit
 
