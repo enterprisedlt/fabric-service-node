646695b1c428e8a89708c917ee07b4d0a154b6c8
Alexey Polubelov <42519530+apolubelov@users.noreply.github.com>
2019-07-19 00:29:29 +0300

    Support for External IP  - added formatting for hosts files

--- service-node/src/main/scala/org/enterprisedlt/fabric/service/node/HostsManager.scala
+++ service-node/src/main/scala/org/enterprisedlt/fabric/service/node/HostsManager.scala
@@ -16,7 +16,9 @@ class HostsManager(
 
     def addOrganization(organization: Organization): Unit = {
         val writer = new FileWriter(hostsFileName, true)
+        val organizationFullName = s"${config.organization.name}.${config.organization.domain}"
         try{
+            writer.append(s"\n#Known hosts for $organizationFullName:\n")
             organization.knownHosts.foreach { host =>
                 writer.append(s"${host.ipAddress}\t${host.dnsName}\n")
             }
