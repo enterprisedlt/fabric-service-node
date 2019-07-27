3aa822bb8aad2621a32772954f8b7192eda5afe2
Maxim Fedin <mfedin1@gmail.com>
2019-07-28 00:35:59 +0300

    some minor changes.

--- fabric-service-start.sh
+++ fabric-service-start.sh
@@ -9,7 +9,7 @@ else
     SERVICE_NODE_HOME=$(dirname "$SCRIPT")
     PROFILE_PATH=$(readlink -f "$1")
 fi
-
+rm -rf ${PROFILE_PATH}/data/*
 . ${PROFILE_PATH}/settings
 echo "Starting Fabric Service Node ..."
 INITIAL_NAME="fabric.service.node.${SERVICE_BIND_PORT}"
@@ -20,19 +20,20 @@ serviceID=`docker run -d \
   -e "SERVICE_NODE_HOME=${SERVICE_NODE_HOME}" \
  -e "SERVICE_EXTERNAL_ADDRESS=${SERVICE_EXTERNAL_ADDRESS}" \
  -e "DOCKER_SOCKET=unix:///host/var/run/docker.sock" \
- -e "LOGS_PATH=/opt/service/data/${INITIAL_NAME}" \
+ -e "LOGS_PATH=/opt/service/data/service" \
  -p ${SERVICE_BIND_PORT}:${SERVICE_BIND_PORT} \
  --volume=${PROFILE_PATH}/hosts:/etc/hosts \
  --volume=${PROFILE_PATH}:/opt/profile \
- --volume=${SERVICE_NODE_HOME}:/opt/service \
+ --volume=${SERVICE_NODE_HOME}/scripts:/opt/service/scripts \
+ --volume=${SERVICE_NODE_HOME}/service-node/build/libs/service-node.jar:/opt/service/service-node/build/libs/service-node.jar \
  --volume=${SERVICE_NODE_HOME}/service-chain-code:/opt/service-chain-code \
- --volume=${PROFILE_PATH}/data/${INITIAL_NAME}:/opt/service/data/${INITIAL_NAME} \
+ --volume=${PROFILE_PATH}/data/service:/opt/service/data/service \
  --volume=/var/run/:/host/var/run/ \
  --name $INITIAL_NAME \
 openjdk:8-jre /opt/service/scripts/with-logs.sh java -jar /opt/service/service-node/build/libs/service-node.jar`
 echo "Service ID: ${serviceID}"
 
 # await service node to start up
-grep -m 1 "ServiceNode\$ - Started" <( tail -f ${PROFILE_PATH}/data/${INITIAL_NAME}/stdout.log)
+grep -m 1 "ServiceNode\$ - Started" <( tail -f ${PROFILE_PATH}/data/service/stdout.log)
 
 echo "======================================================================"
