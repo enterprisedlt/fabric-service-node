54e1eda0be7cafec5c4d81aa3f9a71b73c7a0298
Alexey Polubelov <42519530+apolubelov@users.noreply.github.com>
2019-08-19 22:46:29 +0300

    build-image script fixed

--- build-image.sh
+++ build-image.sh
@@ -6,7 +6,7 @@ gradle service-node:shadowJar
 gradle service-chain-code:shadowJar
 
 # pack chain-code to deploy-able tarball
-./pack-chaincode.sh ./service-chain-code service-chain-code.tgz
+./bin/fabric-service-pack-chaincode.sh ./service-chain-code service-chain-code.tgz
 
 docker build -t enterprisedlt/fabric-service-node:1.4.1 .
 docker tag enterprisedlt/fabric-service-node:1.4.1 enterprisedlt/fabric-service-node
