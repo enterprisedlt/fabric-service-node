88c5bdeb9ea0fc6d7d717b713c9aaa13689571d1
Alexey Polubelov <42519530+apolubelov@users.noreply.github.com>
2019-08-21 01:01:13 +0300

    prepare for 1.4.2-rc-1

--- build-image.sh
+++ build-image.sh
@@ -8,5 +8,5 @@ gradle service-chain-code:shadowJar
 # pack chain-code to deploy-able tarball
 ./bin/fabric-service-pack-chaincode.sh ./service-chain-code service-chain-code.tgz
 
-docker build -t enterprisedlt/fabric-service-node:1.4.1 .
-docker tag enterprisedlt/fabric-service-node:1.4.1 enterprisedlt/fabric-service-node
+docker build -t enterprisedlt/fabric-service-node:1.4.2-rc-1 .
+docker tag enterprisedlt/fabric-service-node:1.4.2-rc-1 enterprisedlt/fabric-service-node
