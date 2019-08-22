224897b69c0f06a4439fbf1559ee7d3ba574ca60
Andrew Pudovikov <pudovikov@me.com>
2019-08-22 19:29:05 +0300

    messaging methods refactored

--- service-chain-code/service/src/main/scala/org/enterprisedlt/fabric/service/operations/MessagingOperations.scala
+++ service-chain-code/service/src/main/scala/org/enterprisedlt/fabric/service/operations/MessagingOperations.scala
@@ -8,27 +8,27 @@ import org.enterprisedlt.fabric.service.model.{CollectionsHelper, Message, Organ
 
 
 /**
-  * @author Andrew Pudovikov
-  */
+ * @author Andrew Pudovikov
+ */
 trait MessagingOperations {
     self: Main.type =>
 
     @ContractOperation
-    def putMessage(context: ContractContext, message: Message): ContractResponse = {
-        val senderOrg = context.clientIdentity.mspId
-        for {
-            _ <- Option(message.to).filter(_.nonEmpty).toRight("Message recipient must be non empty")
-            _ <- Option(message.body).filter(_.nonEmpty).toRight("Message body must be non empty")
-            senderOrganization <- context.store.get[Organization](senderOrg).toRight(s"Sender organization $senderOrg isn't registered")
-            recipientOrganization <- context.store.get[Organization](message.to).toRight(s"Recipient organization ${message.to} isn't registered")
-        } yield {
-            context
-              .privateStore(CollectionsHelper.collectionNameFor(senderOrganization, recipientOrganization))
-              .put(context.transaction.id, message.copy(
-                  from = senderOrganization.mspId,
-                  timestamp = context.transaction.timestamp.toEpochMilli))
-        }
-    }
+    def putMessage(context: ContractContext, message: Message): ContractResponse =
+        getOwnOrganization(context)
+          .flatMap { senderOrganization =>
+              for {
+                  _ <- Option(message.to).filter(_.nonEmpty).toRight("Message recipient must be non empty")
+                  _ <- Option(message.body).filter(_.nonEmpty).toRight("Message body must be non empty")
+                  recipientOrganization <- context.store.get[Organization](message.to).toRight(s"Recipient organization ${message.to} isn't registered")
+              } yield {
+                  context
+                    .privateStore(CollectionsHelper.collectionNameFor(senderOrganization, recipientOrganization))
+                    .put(context.transaction.id, message.copy(
+                        from = senderOrganization.mspId,
+                        timestamp = context.transaction.timestamp.toEpochMilli))
+              }
+          }
 
     @ContractOperation
     def listMessages(context: ContractContext): ContractResponse = {
@@ -38,40 +38,47 @@ trait MessagingOperations {
                   context.store
                     .list[Organization]
                     .map(_.value.mspId))
-              .filter(e => e.endsWith(s"-${context.clientIdentity.mspId}") || e.startsWith(s"${context.clientIdentity.mspId}-"))
+              .filter(e => e.endsWith(s"-${
+                  context.clientIdentity.mspId
+              } ") || e.startsWith(s"${
+                  context.clientIdentity.mspId
+              }-"))
               .map(context.privateStore)
-              .flatMap { store =>
-                  store.list[Message]
+              .flatMap {
+                  store =>
+                      store.list[Message]
               }.toArray
         )
     }
 
     @ContractOperation
-    def getMessage(context: ContractContext, messageKey: String, sender: String): ContractResponse = {
-        val queryingOrg = context.clientIdentity.mspId
-        for {
-            queryingOrganization <- context.store.get[Organization](queryingOrg).toRight(s"Organization $queryingOrg isn't registered")
-            senderOrganization <- context.store.get[Organization](sender).toRight(s"Sender organization $sender isn't registered")
-            message <- context
-              .privateStore(
-                  CollectionsHelper.collectionNameFor(queryingOrganization, senderOrganization)
-              )
-              .get[Message](messageKey)
-              .toRight(s"No message for key $messageKey from $sender")
-        } yield message
-    }
+    def getMessage(context: ContractContext, messageKey: String, sender: String): ContractResponse =
+        getOwnOrganization(context)
+          .flatMap { queryingOrganization =>
+              for {
+                  senderOrganization <- context.store.get[Organization](sender).toRight(s"Sender organization $sender isn't registered")
+                  message <- context
+                    .privateStore(
+                        CollectionsHelper.collectionNameFor(queryingOrganization, senderOrganization)
+                    )
+                    .get[Message](messageKey)
+                    .toRight(s"No message for key $messageKey from $sender ")
+              } yield message
+          }
 
     @ContractOperation
-    def delMessage(context: ContractContext, messageKey: String, sender: String): ContractResponse = {
-        val queryingOrg = context.clientIdentity.mspId
-        for {
-            queryingOrganization <- context.store.get[Organization](queryingOrg).toRight(s"Querying organization $queryingOrg isn't registered")
-            senderOrganization <- context.store.get[Organization](sender).toRight(s"Sender organization $sender isn't registered")
-        } yield
-            context
-              .privateStore(
-                  CollectionsHelper.collectionNameFor(queryingOrganization, senderOrganization)
-              )
-              .del[Message](messageKey)
-    }
+    def delMessage(context: ContractContext, messageKey: String, sender: String): ContractResponse =
+        getOwnOrganization(context)
+          .flatMap { queryingOrganization =>
+              for {
+                  _ <- Option(messageKey).filter(_.nonEmpty).toRight("Message key must be non empty")
+                  senderOrganization <- context.store.get[Organization](sender).toRight(s"Sender organization $sender isn't registered")
+              } yield {
+                  context
+                    .privateStore(
+                        CollectionsHelper.collectionNameFor(queryingOrganization, senderOrganization)
+                    )
+                    .del[Message](messageKey)
+              }
+          }
 }
