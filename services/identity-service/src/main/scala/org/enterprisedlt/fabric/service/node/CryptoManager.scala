86a43a780929e6163eff37df767e0b7106d82cbc
Maxim Fedin <m-fedin@mail.ru>
2019-09-06 19:30:12 +0300

    TODO: rest methods of identity service.

--- services/identity-service/src/main/scala/org/enterprisedlt/fabric/service/node/CryptoManager.scala
+++ services/identity-service/src/main/scala/org/enterprisedlt/fabric/service/node/CryptoManager.scala
@@ -6,12 +6,7 @@ import java.security.KeyStore
   * @author Alexey Polubelov
   */
 trait CryptoManager {
-    // Common
-    //    def createServiceUserKeyStore(name: String, password: String): KeyStore
-    //
-    //    def findUser(user: X509Certificate): Either[String, UserAccount]
 
-    // REST
     def getFabricUserKeyStore(name: String, password: String): KeyStore // /admin/get-user-key
 
     def createFabricUser(name: String): Unit // /admin/create-user
