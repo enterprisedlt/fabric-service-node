f8293a501b26e1d6f77b48540c5a7f5f4bb817f4
Maxim Fedin <m-fedin@mail.ru>
2019-09-07 19:08:53 +0300

    interfaces moved to common.

---
+++ common/src/main/scala/org/enterprisedlt/fabric/service/node/services/CryptoManager.scala
@@ -0,0 +1,13 @@
+package org.enterprisedlt.fabric.service.node.services
+
+import java.security.KeyStore
+
+/**
+  * @author Alexey Polubelov
+  */
+trait CryptoManager {
+
+    def getFabricUserKeyStore(name: String, password: String): KeyStore // /admin/get-user-key
+
+    def createFabricUser(name: String): Unit // /admin/create-user
+}
