b7fff84846a303c4c2a7467d09acabe9800d3720
Maxim Fedin <m-fedin@mail.ru>
2019-09-07 19:29:39 +0300

    ProcessManagement endpoint initialized. TODO: to fill those endpoints.

--- services/process-management-service/src/main/scala/org/enterprisedlt/fabric/service/node/ProcessManagementEndpoint.scala
+++ services/process-management-service/src/main/scala/org/enterprisedlt/fabric/service/node/ProcessManagementEndpoint.scala
@@ -1,8 +1,26 @@
 package org.enterprisedlt.fabric.service.node
 
+import org.enterprisedlt.fabric.service.node.process.DockerBasedProcessManager
+
+
 /**
   * @author Maxim Fedin
   */
-object ProcessManagementNode extends App{
+class ProcessManagementEndpoint(processManager: DockerBasedProcessManager) {
 
+
+
+// TODO this Endpoints:
+
+/*    def startOrderingNode(name: String): Either[String, String]
+
+    def osnAwaitJoinedToRaft(name: String): Unit
+
+    def osnAwaitJoinedToChannel(name: String, channelName: String): Unit
+
+    def startPeerNode(name: String): Either[String, String]
+
+    def peerAwaitForBlock(name: String, blockNumber: Long): Unit
+
+    def terminateChainCode(peerName: String, chainCodeName: String, chainCodeVersion: String): Unit*/
 }
--- services/process-management-service/src/main/scala/org/enterprisedlt/fabric/service/node/ProcessManagementNode.scala
+++ services/process-management-service/src/main/scala/org/enterprisedlt/fabric/service/node/ProcessManagementNode.scala
@@ -1,6 +1,59 @@
+package org.enterprisedlt.fabric.service.node
+
+import java.io.FileReader
+
+import org.enterprisedlt.fabric.service.node.configuration.ServiceConfig
+import org.enterprisedlt.fabric.service.node.process.DockerBasedProcessManager
+import org.enterprisedlt.fabric.service.node.rest.JsonRestEndpoint
+import org.enterprisedlt.fabric.service.node.util.Util
+import org.slf4j.LoggerFactory
+
 /**
   * @author Maxim Fedin
   */
-object ProcessManagementNode extends App{
+object ProcessManagementNode extends App {
+    private val Environment = System.getenv()
+    private val LogLevel = Option(Environment.get("LOG_LEVEL")).filter(_.trim.nonEmpty).getOrElse("INFO")
+    private val ProcessManagementBindPort = Option(Environment.get("PROCESS_MANAGEMENT_BIND_PORT")).map(_.toInt).getOrElse(throw new Exception("Mandatory environment variable missing SERVICE_BIND_PORT!"))
+    private val ProfilePath = Option(Environment.get("PROFILE_PATH")).getOrElse(throw new Exception("Mandatory environment variable missing PROFILE_PATH!"))
+    private val DockerSocket = Option(Environment.get("DOCKER_SOCKET")).getOrElse(throw new Exception("Mandatory environment variable missing DOCKER_SOCKET!"))
+    private val InitialName = Option(Environment.get("INITIAL_NAME")).getOrElse(throw new Exception("Mandatory environment variable missing INITIAL_NAME!"))
+
+    Util.setupLogging(LogLevel)
+    private val logger = LoggerFactory.getLogger(this.getClass)
+    private val cryptoPath = "/opt/profile/crypto"
+    logger.info("Starting...")
+    private val config = loadConfig("/opt/profile/service.json")
+
+    private val processManager = new DockerBasedProcessManager(
+        ProfilePath, DockerSocket,
+        InitialName, config
+    )
+
+
+    private val restEndpoint = new JsonRestEndpoint(ProcessManagementBindPort, new ProcessManagementEndpoint(processManager))
+
+    setupShutdownHook()
+    restEndpoint.start()
+    logger.info("Started.")
+
+    //=========================================================================
+    private def setupShutdownHook(): Unit = {
+        Runtime.getRuntime.addShutdownHook(new Thread("shutdown-hook") {
+            override def run(): Unit = {
+                logger.info("Shutting down...")
+                restEndpoint.stop()
+                logger.info("Shutdown complete.")
+            }
+        })
+    }
+
+    private def loadConfig(configFile: String): ServiceConfig =
+        Util.codec.fromJson(new FileReader(configFile), classOf[ServiceConfig])
 
 }
+
+case class ExternalAddress(
+    host: String,
+    port: Int
+)
