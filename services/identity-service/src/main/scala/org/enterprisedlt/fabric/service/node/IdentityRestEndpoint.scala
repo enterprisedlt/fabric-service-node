190c0e75532ff14180716460da89976d66c81a57
Maxim Fedin <m-fedin@mail.ru>
2019-09-07 16:53:42 +0300

    two rest points added to IdentityRestEndpoint.

--- services/identity-service/src/main/scala/org/enterprisedlt/fabric/service/node/IdentityRestEndpoint.scala
+++ services/identity-service/src/main/scala/org/enterprisedlt/fabric/service/node/IdentityRestEndpoint.scala
@@ -1,5 +1,8 @@
 package org.enterprisedlt.fabric.service.node
 
+import javax.servlet.http.HttpServletResponse
+import org.apache.http.entity.ContentType
+import org.enterprisedlt.fabric.service.node.rest.{Get, Post, RestEndpointContext}
 import org.slf4j.LoggerFactory
 
 /**
@@ -8,22 +11,29 @@ import org.slf4j.LoggerFactory
 class IdentityRestEndpoint(cryptoManager: CryptoManager) {
     private val logger = LoggerFactory.getLogger(this.getClass)
 
+    @Get("/services/identity/get-user-key")
+    def getUserKey(): Either[String, Unit] = {
+        RestEndpointContext.get.map { context =>
+            val userName = context.request.getParameter("name")
+            val password = context.request.getParameter("password")
+            logger.info(s"Obtaining user key for $userName ...")
+            val key = cryptoManager.getFabricUserKeyStore(userName, password)
+            context.response.setContentType(ContentType.APPLICATION_OCTET_STREAM.getMimeType)
+            key.store(context.response.getOutputStream, password.toCharArray)
+            context.response.setStatus(HttpServletResponse.SC_OK)
+        }.toRight("Not context!")
+    }
 
-    //    case "/admin/create-user" =>
-    //    val userName = request.getParameter("name")
-    //    logger.info(s"Creating new user $userName ...")
-    //    cryptoManager.createFabricUser(userName)
-    //    response.setContentType(ContentType.TEXT_PLAIN.getMimeType)
-    //    response.getWriter.println("OK")
-    //    response.setStatus(HttpServletResponse.SC_OK)
-    //
-    //    case "/admin/get-user-key" =>
-    //    val userName = request.getParameter("name")
-    //    val password = request.getParameter("password")
-    //    logger.info(s"Obtaining user key for $userName ...")
-    //    val key = cryptoManager.getFabricUserKeyStore(userName, password)
-    //    response.setContentType(ContentType.APPLICATION_OCTET_STREAM.getMimeType)
-    //    key.store(response.getOutputStream, password.toCharArray)
-    //    response.setStatus(HttpServletResponse.SC_OK)
+    @Post("/services/identity/create-user")
+    def createUser(): Either[String, Unit] = {
+        RestEndpointContext.get.map { context =>
+            val userName = context.request.getParameter("name")
+            logger.info(s"Creating new user $userName ...")
+            cryptoManager.createFabricUser(userName)
+            context.response.setContentType(ContentType.TEXT_PLAIN.getMimeType)
+            context.response.getWriter.println("OK")
+            context.response.setStatus(HttpServletResponse.SC_OK)
+        }
+    }.toRight("Not context!")
 
 }
