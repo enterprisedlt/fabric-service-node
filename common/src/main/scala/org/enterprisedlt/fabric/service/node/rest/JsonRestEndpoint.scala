a00da34e5667e2b2e03ecbd5489421cbd1f49ce2
Maxim Fedin <m-fedin@mail.ru>
2019-09-09 18:35:59 +0300

    JsonRestClient has been added. TODO: to adopt current services to the updated REST service.

--- common/src/main/scala/org/enterprisedlt/fabric/service/node/rest/JsonRestEndpoint.scala
+++ common/src/main/scala/org/enterprisedlt/fabric/service/node/rest/JsonRestEndpoint.scala
@@ -1,29 +1,22 @@
 package org.enterprisedlt.fabric.service.node.rest
 
+import java.io.{Reader, Writer}
 import java.lang.reflect.Method
 
-import com.google.gson.{Gson, GsonBuilder}
 import javax.servlet.http.{HttpServletRequest, HttpServletResponse}
 import org.apache.http.entity.ContentType
+import org.eclipse.jetty.server.Request
 import org.eclipse.jetty.server.handler.AbstractHandler
-import org.eclipse.jetty.server.{Request, Server}
 import org.slf4j.LoggerFactory
 
 /**
- * @author Alexey Polubelov
- */
+  * @author Alexey Polubelov
+  */
 class JsonRestEndpoint(
-    port: Int,
+    createCodec: () => Codec,
     endpoints: AnyRef*
-) {
+) extends AbstractHandler {
     private val logger = LoggerFactory.getLogger(getClass)
-    private val server: Server = createServer
-
-    private def createServer: Server = {
-        val server = new Server(port)
-        server.setHandler(EndpointHandler)
-        server
-    }
 
     private val GetBindings = endpoints.flatMap { spec =>
         scanMethods(spec.getClass)
@@ -47,12 +40,6 @@ class JsonRestEndpoint(
           }
     }.toMap
 
-    def start(): Unit = server.start()
-
-    def stop(): Unit = server.stop()
-
-    private def getCodec: Gson = new GsonBuilder().setPrettyPrinting().create()
-
     private def scanMethods(c: Class[_]): Array[Method] = {
         c.getMethods ++
           c.getInterfaces.flatMap(scanMethods) ++
@@ -60,33 +47,34 @@ class JsonRestEndpoint(
     }
 
     type HandleFunction = (HttpServletRequest, HttpServletResponse) => Unit
-    type ExtractParametersFunction = (Method, HttpServletRequest, Gson) => Seq[AnyRef]
+    type ExtractParametersFunction = (Method, HttpServletRequest, Codec) => Seq[AnyRef]
 
     private def createHandler(o: AnyRef, m: Method, parametersFunction: ExtractParametersFunction): (HttpServletRequest, HttpServletResponse) => Unit = {
+        logger.info(s"Creating handler for: ${m.getName}")
         val eClazz = classOf[Either[String, Any]]
         m.getReturnType match {
             case x if x.isAssignableFrom(eClazz) =>
                 val f: (HttpServletRequest, HttpServletResponse) => Unit = { (request, response) =>
                     try {
-                        val codec = getCodec
+                        val codec = createCodec()
                         val params: Seq[AnyRef] = parametersFunction(m, request, codec)
                         m.invoke(o, params: _*) match {
                             case Right(value) =>
-                                if (response.getContentType == null) { // if content type already set do NOT overwrite
-                                    response.setContentType(ContentType.APPLICATION_JSON.getMimeType)
-                                }
-                                Option(value).filterNot(_.isInstanceOf[Unit]).foreach(v => response.getWriter.print(codec.toJson(v)))
+                                Option(value)
+                                  .filterNot(_.isInstanceOf[Unit])
+                                  .foreach(v => codec.writeResult(v, response.getWriter))
+
+                                response.setContentType(
+                                    Option(m.getAnnotation(classOf[ResponseContentType]))
+                                      .map(_.value())
+                                      .getOrElse(ContentType.APPLICATION_JSON.getMimeType)
+                                )
                                 response.setStatus(HttpServletResponse.SC_OK)
 
                             case Left(errorMsg) =>
                                 response.setContentType(ContentType.TEXT_PLAIN.getMimeType)
                                 response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR)
-                                response.getWriter.print(errorMsg)
-
-                            case _ =>
-                                response.setContentType(ContentType.TEXT_PLAIN.getMimeType)
-                                response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR)
-
+                                codec.writeResult(errorMsg, response.getWriter)
                         }
                     } catch {
                         case ex: Throwable =>
@@ -103,50 +91,55 @@ class JsonRestEndpoint(
         }
     }
 
-    private def mkPostParams(m: Method, request: HttpServletRequest, codec: Gson): Seq[AnyRef] = {
+    private def mkPostParams(m: Method, request: HttpServletRequest, codec: Codec): Seq[AnyRef] = {
         m.getParameters.headOption.map { p =>
-            codec.fromJson(request.getReader, p.getType).asInstanceOf[AnyRef]
+            codec.readParameter(request.getReader, p.getType).asInstanceOf[AnyRef]
         }.toSeq
     }
 
-    private def mkGetParams(m: Method, request: HttpServletRequest, codec: Gson): Seq[AnyRef] = {
+    private def mkGetParams(m: Method, request: HttpServletRequest, codec: Codec): Seq[AnyRef] = {
         m.getParameters.map { p =>
             val v = Option(request.getParameter(p.getName)).getOrElse(throw new Exception("mandatory parameter absent"))
-            codec.fromJson(v, p.getType).asInstanceOf[AnyRef]
+            codec.readParameter(v, p.getType).asInstanceOf[AnyRef]
         }
     }
 
-    private object EndpointHandler extends AbstractHandler {
+    override def handle(target: String, baseRequest: Request, request: HttpServletRequest, response: HttpServletResponse): Unit = {
+        RestEndpointContext.set(RestEndpointContext(request, response))
+        request.getMethod match {
+            case "GET" =>
+                processRequest(GetBindings, request, response)
 
-        override def handle(target: String, baseRequest: Request, request: HttpServletRequest, response: HttpServletResponse): Unit = {
-            RestEndpointContext.set(RestEndpointContext(request, response))
-            request.getMethod match {
-                case "GET" =>
-                    processRequest(GetBindings, request, response)
+            case "POST" =>
+                processRequest(PostBindings, request, response)
 
-                case "POST" =>
-                    processRequest(PostBindings, request, response)
+            case methodName =>
+                response.getWriter.println(s"Unsupported method type $methodName")
+                response.setStatus(HttpServletResponse.SC_BAD_REQUEST)
 
-                case methodName =>
-                    response.getWriter.println(s"Unsupported method type $methodName")
-                    response.setStatus(HttpServletResponse.SC_BAD_REQUEST)
-
-            }
-            RestEndpointContext.clean()
-            baseRequest.setHandled(true)
         }
+        RestEndpointContext.clean()
+        baseRequest.setHandled(true)
+    }
 
-        private def processRequest(
-            bindings: Map[String, HandleFunction],
-            request: HttpServletRequest, response: HttpServletResponse
-        ): Unit = {
-            bindings.get(request.getPathInfo) match {
-                case Some(f) =>
-                    f(request, response)
-                case None =>
-                    response.setStatus(HttpServletResponse.SC_NOT_FOUND)
-            }
+    private def processRequest(
+        bindings: Map[String, HandleFunction],
+        request: HttpServletRequest, response: HttpServletResponse
+    ): Unit = {
+        bindings.get(request.getPathInfo) match {
+            case Some(f) =>
+                f(request, response)
+            case None =>
+                response.setStatus(HttpServletResponse.SC_NOT_FOUND)
         }
     }
 
+}
+
+trait Codec {
+    def writeResult(x: Any, stream: Writer)
+
+    def readParameter[T](p: String, pType: Class[T]): T
+
+    def readParameter[T](p: Reader, pType: Class[T]): T
 }
