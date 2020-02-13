594f6b6c13d4ae6e2730e6c864c0a71f9ca7b2a1
Alexey Polubelov <42519530+apolubelov@users.noreply.github.com>
2020-02-13 11:59:23 +0300

    migration to sbt

--- service-chain-code/service/src/main/scala/org/enterprisedlt/fabric/service/operations/ContractInitialize.scala
+++ service-chain-code/service/src/main/scala/org/enterprisedlt/fabric/service/operations/ContractInitialize.scala
@@ -2,16 +2,16 @@ package org.enterprisedlt.fabric.service.operations
 
 import org.enterprisedlt.fabric.service.Main
 import org.enterprisedlt.fabric.service.model.{Organization, ServiceVersion}
-import org.enterprisedlt.spec.ContractInit
+import org.enterprisedlt.spec.{ContractInit, ContractResult}
 
 /**
-  * @author Andrew Pudovikov
-  */
+ * @author Andrew Pudovikov
+ */
 trait ContractInitialize {
     self: Main.type =>
 
     @ContractInit
-    def init(organization: Organization, version: ServiceVersion): Unit = {
+    def init(organization: Organization, version: ServiceVersion): ContractResult[Unit] = {
         putOrganization(organization)
         updateServiceVersion(version)
     }
