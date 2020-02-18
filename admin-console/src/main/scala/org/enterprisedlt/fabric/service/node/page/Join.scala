d47f1158b7c630aaa04f0f769f811f74b5e2d0c0
Maxim Fedin <m-fedin@mail.ru>
2020-02-18 12:56:20 +0300

    Join has been initialized.

---
+++ admin-console/src/main/scala/org/enterprisedlt/fabric/service/node/page/Join.scala
@@ -0,0 +1,47 @@
+package org.enterprisedlt.fabric.service.node.page
+
+import japgolly.scalajs.react.component.Scala.Unmounted
+import japgolly.scalajs.react.vdom.html_<^._
+import japgolly.scalajs.react.{BackendScope, Callback, ScalaComponent}
+import org.enterprisedlt.fabric.service.node.{Context, Initial}
+import org.scalajs.dom.html.Div
+
+/**
+  * @author Maxim Fedin
+  */
+object Join {
+
+    case class JoinSettings(
+        //TODO
+    )
+
+    private val component = ScalaComponent.builder[Unit]("JoinMode")
+      .initialState(JoinSettings())
+      .renderBackend[Backend]
+      .build
+
+
+    class Backend(val $: BackendScope[Unit, JoinSettings]) {
+
+        def goInit: Callback = Callback {
+            Context.State.update(_ => Initial)
+        }
+
+        def goBootProgress: Callback = Callback() // TODO
+
+        def render(s: JoinSettings): VdomTagOf[Div] =
+            <.div(^.className := "card aut-form-card",
+                <.div(^.className := "card-header text-white bg-primary",
+                    <.h1("Bootstrap new network")
+                ),
+                <.hr(),
+                <.div(^.className := "form-group mt-1",
+                    <.button(^.`type` := "button", ^.className := "btn btn-outline-secondary", ^.onClick --> goInit, "Back"),
+                    <.button(^.`type` := "button", ^.className := "btn btn-outline-success float-right", ^.onClick --> goBootProgress, "Bootstrap")
+                )
+                //                                                                                                        </form>
+            )
+    }
+    def apply(): Unmounted[Unit, JoinSettings, Backend] = component()
+
+}
