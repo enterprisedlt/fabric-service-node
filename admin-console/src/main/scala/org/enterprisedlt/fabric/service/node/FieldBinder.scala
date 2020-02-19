5a3a702c232d94bf05f52dc422ddfa6aaf99e714
Alexey Polubelov <42519530+apolubelov@users.noreply.github.com>
2020-02-19 14:20:54 +0300

    added value converters for bind

--- admin-console/src/main/scala/org/enterprisedlt/fabric/service/node/FieldBinder.scala
+++ admin-console/src/main/scala/org/enterprisedlt/fabric/service/node/FieldBinder.scala
@@ -5,6 +5,9 @@ import japgolly.scalajs.react.vdom.html_<^._
 import japgolly.scalajs.react.{BackendScope, Callback, ReactEventFromInput}
 import monocle.Lens
 
+
+import scala.util.Try
+
 /**
  * @author Alexey Polubelov
  */
@@ -13,11 +16,17 @@ trait FieldBinder[S] {
 
     def bind(v: S): Bind = new Bind(v)
 
-    private def mapInputTo[X](l: Lens[S, X])(event: ReactEventFromInput): Callback = {
+    private def mapInputTo[X](l: Lens[S, X])(converter: String => Option[X])(event: ReactEventFromInput): Callback = {
         val v: String = event.target.value
-        $.modState(l.set(v.asInstanceOf[X]))
+        // try convert, if not simply ignore
+        converter(v)
+          .map { x =>
+              $.modState(l.set(x))
+          }
+          .getOrElse(Callback(println(s"Invalid value: $v")))
     }
 
+    import scala.language.higherKinds
     implicit class ComposeLens[A, B](self: Lens[A, B]) {
 
         def /[D](other: Lens[B, D]): Lens[A, D] =
@@ -36,10 +45,16 @@ trait FieldBinder[S] {
             }
     }
 
+    //
+    implicit val String2String: String => Option[String] = x => Option(x)
+    implicit val String2Int: String => Option[Int] = x => Try(x.toInt).toOption
+
+
     class Bind(s: S) {
-        def :=[X](l: Lens[S, X]): TagMod = {
+
+        def :=[X](l: Lens[S, X])(implicit converter: String => Option[X]): TagMod = {
             Seq(
-                ^.onChange ==> mapInputTo(l),
+                ^.onChange ==> mapInputTo(l)(converter),
                 ^.value := l.get(s).toString
             ).toTagMod
         }
