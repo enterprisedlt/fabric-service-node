3905a4b8aed099b4fec8d896b9bb43ef48469936
Maxim Fedin <m-fedin@mail.ru>
2020-02-20 16:41:35 +0300

    Add forms for a Dashboard's add organization

--- admin-console/src/main/scala/org/enterprisedlt/fabric/service/node/page/Dashboard.scala
+++ admin-console/src/main/scala/org/enterprisedlt/fabric/service/node/page/Dashboard.scala
@@ -2,7 +2,7 @@ package org.enterprisedlt.fabric.service.node.page
 
 import japgolly.scalajs.react.component.Scala.Unmounted
 import japgolly.scalajs.react.vdom.html_<^._
-import japgolly.scalajs.react.{BackendScope, Callback, ScalaComponent}
+import japgolly.scalajs.react.{BackendScope, Callback, CallbackTo, ReactEventFromInput, ScalaComponent}
 import org.enterprisedlt.fabric.service.node.connect.ServiceNodeRemote
 import org.enterprisedlt.fabric.service.node.util.Tags._
 import org.scalajs.dom
@@ -13,8 +13,8 @@ import scala.concurrent.ExecutionContext.Implicits.global
 import scala.scalajs.js
 
 /**
- * @author Alexey Polubelov
- */
+  * @author Alexey Polubelov
+  */
 object Dashboard {
 
     case class State()
@@ -32,20 +32,121 @@ object Dashboard {
             }
         }
 
+
+        def addFile(event: ReactEventFromInput): CallbackTo[Unit] = ???
+
         def render(s: State): VdomTagOf[Div] =
             <.div(
                 Tabs(
                     ("organizations", "Organizations",
-                      <.div(
+                      <.div(^.className := "card-body aut-form-card",
+                          <.h4("Invite organization"),
+                          <.div(^.float.right, ^.verticalAlign.`text-top`,
+                              <.button(^.tpe := "button", ^.className := "btn btn-outline-secondary", ^.onClick --> createInvite, "Invite organization")
+                          ),
                           <.ul(
                               <.li("Org1"),
                               <.li("Org2")
                           ),
+
+                          <.hr(),
+                          <.h4("Add organization"),
                           <.div(^.float.right, ^.verticalAlign.`text-top`,
-                              <.button(^.tpe := "button", ^.className := "btn btn-outline-secondary", ^.onClick --> createInvite, "Invite organization")
-                          )
+                              <.button(^.tpe := "button", ^.className := "btn btn-outline-secondary",  "Add organization")
+                          ),
+
+                          <.span(<.br()),
+                          <.h5("Certificates"),
+
+                          <.div(^.className := "form-group row",
+                              <.label(^.className := "col-sm-2 col-form-label", "Ca Cert:"),
+                              <.div(^.className := "input-group col-sm-10",
+                                  <.div(^.`class` := "custom-file",
+                                      <.input(^.`type` := "file", ^.`class` := "custom-file-input", ^.id := "caCerts", ^.onChange ==> addFile),
+                                      <.label(^.`class` := "custom-file-label", "Choose file")
+                                  )
+                              )
+                          ),
+                          <.div(^.className := "form-group row",
+                              <.label(^.className := "col-sm-2 col-form-label", "TLS Ca Cert:"),
+                              <.div(^.className := "input-group col-sm-10",
+                                  <.div(^.`class` := "custom-file",
+                                      <.input(^.`type` := "file", ^.`class` := "custom-file-input", ^.id := "tlsCACerts", ^.onChange ==> addFile),
+                                      <.label(^.`class` := "custom-file-label", "Choose file")
+                                  )
+                              )
+                          ),
+                          <.div(^.className := "form-group row",
+                              <.label(^.className := "col-sm-2 col-form-label", "Admin cert:"),
+                              <.div(^.className := "input-group col-sm-10",
+                                  <.div(^.`class` := "custom-file",
+                                      <.input(^.`type` := "file", ^.`class` := "custom-file-input", ^.id := "adminCerts", ^.onChange ==> addFile),
+                                      <.label(^.`class` := "custom-file-label", "Choose file")
+                                  )
+                              )
+                          ),
+                          <.hr(),
+                          <.h5("Organization Info"),
+
+                          <.div(^.className := "form-group row",
+                              <.label(^.className := "col-sm-2 col-form-label", "Organization msp Id"),
+                              <.div(^.className := "col-sm-10",
+                                  <.input(^.`type` := "text", ^.className := "form-control",
+                                  )
+                              )
+                          ),
+
+                          <.div(^.className := "form-group row",
+                              <.label(^.className := "col-sm-2 col-form-label", "Organization name"),
+                              <.div(^.className := "col-sm-10",
+                                  <.input(^.`type` := "text", ^.className := "form-control",
+                                  )
+                              )
+                          ),
+                          <.div(^.className := "form-group row",
+                              <.label(^.className := "col-sm-2 col-form-label", "Member number"),
+                              <.div(^.className := "col-sm-10",
+                                  <.input(^.`type` := "text", ^.className := "form-control",
+                                  )
+                              )
+                          ),
+                          <.hr(),
+                          <.h5("Known hosts"),
+
+                          <.div(^.className := "form-group row",
+                              <.table(^.className := "table table-hover table-sm",
+                                  <.thead(
+                                      <.tr(
+                                          <.th(^.scope := "col", "#"),
+                                          <.th(^.scope := "col", "IP address"),
+                                          <.th(^.scope := "col", "DNS name"),
+                                          <.th(^.scope := "col", "Actions"),
+                                      )
+                                  ),
+                                  <.tbody()
+                              )
+                          ),
+
+                          <.div(^.className := "form-group row",
+                              <.label(^.`for` := "componentName", ^.className := "col-sm-2 col-form-label", "IP address"),
+                              <.div(^.className := "col-sm-10",
+                                  <.input(^.`type` := "text", ^.className := "form-control", ^.id := "ipAddress")
+                              )),
+                          <.div(^.className := "form-group row",
+                              <.label(^.`for` := "port", ^.className := "col-sm-2 col-form-label", "DNS name"),
+                              <.div(^.className := "col-sm-10",
+                                  <.input(^.`type` := "text", ^.className := "form-control", ^.id := "dnsName"))),
+                          <.div(^.className := "form-group row",
+                              <.button(
+                                  ^.className := "btn btn-primary",
+                                  "Add host")
+                          ),
+
+
                       )
                     ),
+
+
                     ("users", "Users",
                       <.div(
 
