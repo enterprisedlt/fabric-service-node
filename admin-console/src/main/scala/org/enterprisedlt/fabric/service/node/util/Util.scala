1afac747453e8085b096a701d18424c55806b88d
Maxim Fedin <m-fedin@mail.ru>
2020-02-27 19:56:34 +0300

    Datetime added.

--- admin-console/src/main/scala/org/enterprisedlt/fabric/service/node/util/Util.scala
+++ admin-console/src/main/scala/org/enterprisedlt/fabric/service/node/util/Util.scala
@@ -6,6 +6,6 @@ import scala.scalajs.js.Date
   * @author Maxim Fedin
   */
 object Util {
-    def mkDate(timestamp: Long): String = new Date(timestamp).formatted("yyyy-MM-dd HH:mm:ss z")
+    def mkDate(timestamp: Long): String = new Date(timestamp * 1000L).toLocaleDateString()
 
 }
