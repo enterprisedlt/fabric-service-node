fd4a00f5db20369f7589f0d57e92f161fca4ebb6
Maxim Fedin <m-fedin@mail.ru>
2020-05-19 14:24:19 +0300

    Test for untarFile method has been added.

---
+++ service-node/backend/src/test/scala/org/enterprisedlt/fabric/service/node/UtilTest.scala
@@ -0,0 +1,57 @@
+package org.enterprisedlt.fabric.service.node
+
+import java.io.{BufferedOutputStream, File, FileInputStream, FileOutputStream}
+import java.nio.charset.StandardCharsets
+import java.nio.file.{Files, Paths}
+
+import org.apache.commons.compress.archivers.tar.{TarArchiveEntry, TarArchiveOutputStream}
+import org.apache.commons.compress.compressors.gzip.GzipCompressorOutputStream
+import org.apache.commons.compress.utils.IOUtils
+import org.scalatest._
+
+/**
+ * @author Andrew Pudovikov
+ */
+class UtilTest extends FunSuite {
+
+
+
+    def tarFile(file: File): File = {
+        val tarZippedFile: File = File.createTempFile("tar-zipped-file", ".tgz")
+        val tarOut = new TarArchiveOutputStream(
+            new GzipCompressorOutputStream(
+                new BufferedOutputStream(
+                    new FileOutputStream(tarZippedFile))))
+        tarOut.putArchiveEntry(new TarArchiveEntry(file, file.getName));
+        IOUtils.copy(new FileInputStream(file), tarOut);
+        tarOut.closeArchiveEntry()
+        tarOut.close()
+        tarZippedFile
+    }
+
+
+    test("untarFile method works fine") {
+        val tempFolder = Files.createTempDirectory("temp-dir").toFile
+        val initialFile = File.createTempFile("test", ".txt", tempFolder)
+        initialFile.deleteOnExit()
+        //
+        val out = new FileOutputStream(initialFile)
+        out.write("Test message".getBytes(StandardCharsets.UTF_8))
+        out.close()
+        //
+        val zippedFile: File = tarFile(initialFile)
+        val zippedFileBytes = Files.readAllBytes(Paths.get(zippedFile.getPath))
+        //
+        Util.untarFile(zippedFileBytes, tempFolder.getAbsolutePath)
+        //
+        val unzippedFileByteArray = Files.readAllBytes(Paths.get(tempFolder.listFiles().head.getAbsolutePath))
+        val unzippedFileContent = new String(unzippedFileByteArray, StandardCharsets.UTF_8)
+        //
+        val initialFileByteArray = Files.readAllBytes(Paths.get(initialFile.getPath))
+        val initialFileContent = new String(initialFileByteArray, StandardCharsets.UTF_8)
+        //
+        assert(unzippedFileContent == initialFileContent)
+    }
+
+
+}
