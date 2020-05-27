49e917325c0c2c2e16346a067c07cb8016407678
Andrew Pudovikov <pudovikov@me.com>
2020-05-27 15:44:40 +0300

    have made explicit choose for the fsn version during build

--- build-image.sh
+++ build-image.sh
@@ -2,10 +2,12 @@
 set -e
 
 FABRIC_SERVICE_NODE_VERSION=$1
-
+if [[ -n $FABRIC_SERVICE_NODE_VERSION ]]; then
 ./build.sh
 ./prepare-scripts.sh ${FABRIC_SERVICE_NODE_VERSION}
 
 docker build -t enterprisedlt/fabric-service-node:${FABRIC_SERVICE_NODE_VERSION} .
 docker tag enterprisedlt/fabric-service-node:${FABRIC_SERVICE_NODE_VERSION} enterprisedlt/fabric-service-node
-
+else
+  echo "Please provide fabric service node version as first opt"
+fi
